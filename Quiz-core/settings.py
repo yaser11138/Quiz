11f97d944139f93d2396bcaca1c0bec0841c710f
yaser <ydahgan@gmail.com>
2022-07-15 13:42:49 +0430

    Add README and requirements

--- Quiz-core/settings.py
+++ Quiz-core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7^s)y&%tn9pqszb)i1@24ugwb&#-*##4m!@47hghpj^bt_q&^!'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
